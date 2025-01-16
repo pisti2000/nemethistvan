d73805aa49609c10ed68753c4044b86f5514a80d
pisti2000 <nemist186@hengersor.hu>
2025-01-16 14:59:01 +0100

    Update index.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -9,3 +9,16 @@ const server = createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+/* function(__dirname, __filename, modul, require, exports) {
+  console-log(__dirname);
+  console.log(__filename)
+  const add = require("./calculator.js");
+  console.log(add(2,3));
+  const http = require("http");
+  
+}
+ const server = hhtp.creatserver(funciton(red,res)){
+ request:
+ URL pl http://localhost:8080/<path>
+ Method pl GET, POST, DELETE, Patch 
+*/
