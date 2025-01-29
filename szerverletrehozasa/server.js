503beff0d9447c02e41a27ad7056295125fd065a
pisti2000 <nemist186@hengersor.hu>
2025-01-29 11:18:20 +0100

    backend
    backend

--- szerverletrehozasa/server.js
+++ szerverletrehozasa/server.js
@@ -2,18 +2,23 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mysql = require("mysql");
-const bodyparser = require('body-parser');
-app.use(bodyparser.json());
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+ 
 app.use(cors());
-
+ 
 const db = mysql.createConnection({
-    user:"root",
+    user: "root",
     host: "127.0.0.1",
-    port:3307,
-    password:"",
-    database:"kozutak", 
+    port: 3307,
+    password: "",
+    database: "kozutak",
 });
-app.listen(3301,() => {
-    console.log("server is running on port 3001")
+ 
+app.get("/", (req,res) => {
+    res.send("Fut a backend");
 })
+ 
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
